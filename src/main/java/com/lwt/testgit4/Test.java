f9f373701d7c502ca3e527c86c42235980e7f51d
liuwentao <liu.wentao@immomo.com>
2021-03-18 19:37:22 +0800

    t1

--- src/main/java/com/lwt/testgit4/Test.java
+++ src/main/java/com/lwt/testgit4/Test.java
@@ -7,13 +7,5 @@ package com.lwt.testgit4;
 public class Test {
     public static void main(String[] args) {
         System.out.println("m1");
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println("m2");
-        System.out.println("t1");
-        System.out.println("m3");
-        System.out.println("t2");
-        System.out.println("t3");
-        System.out.println("t5");
     }
 }
