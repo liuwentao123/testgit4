9cb3f801a918bc425759d2ea3e0c5fbcc17b27f5
liuwentao <liu.wentao@immomo.com>
2021-03-18 20:10:27 +0800

    t23

--- src/main/java/com/lwt/testgit4/Test.java
+++ src/main/java/com/lwt/testgit4/Test.java
@@ -8,6 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("t21");
         System.out.println("t22");
+        System.out.println("t23");
 
     }
 }
