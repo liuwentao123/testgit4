fab15c83e1bc734c5902710ea3427502181e208b
liuwentao <liu.wentao@immomo.com>
2021-03-20 20:18:35 +0800

    211

--- src/main/java/com/lwt/testgit4/Test.java
+++ src/main/java/com/lwt/testgit4/Test.java
@@ -6,14 +6,6 @@ package com.lwt.testgit4;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("t21");
-        System.out.println("t22");
-        System.out.println("t23");
-        System.out.println("t24");
-        System.out.println("t25");
-        System.out.println("t26");
-        System.out.println("t27");
-        System.out.println("t28");
-        System.out.println("t29");
+        System.out.println("t211");
     }
 }
