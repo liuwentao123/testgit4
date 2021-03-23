0661afdf0bc158ded2cd3b93ce87ce5eac3e37ba
liuwentao <liu.wentao@immomo.com>
2021-03-23 13:58:08 +0800

    1

--- src/main/java/com/lwt/testgit4/Test.java
+++ src/main/java/com/lwt/testgit4/Test.java
@@ -6,37 +6,6 @@ package com.lwt.testgit4;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("t211");
-        System.out.println("t212");
-        System.out.println("t213");
-        System.out.println("t21");
-        System.out.println("t22");
-        System.out.println("t23");
-        System.out.println("t24");
-        System.out.println("t25");
-        System.out.println("t26");
-        System.out.println("t27");
-        System.out.println("t28");
-        System.out.println("t29");
-        System.out.println("t30");
-        System.out.println("t31");
-        System.out.println("t32");
-        System.out.println("t33");
-        System.out.println("t34");
-        System.out.println("t35");
-        System.out.println("t36");
-        System.out.println("t37");
-        System.out.println("t38");
-        System.out.println("t39");
-        System.out.println("t40");
-        System.out.println("t41");
-        System.out.println("t42");
-        System.out.println("t43");
-        System.out.println("t44");
-        System.out.println("t45");
-        System.out.println("t46");
-        System.out.println("t47");
-        System.out.println("t48");
-        System.out.println("t49");
+        System.out.println("1");
     }
 }
