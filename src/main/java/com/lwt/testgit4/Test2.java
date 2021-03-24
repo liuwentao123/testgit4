72457b5de3707c294e658bfbdc2b2eb88eff9322
liuwentao <liu.wentao@immomo.com>
2021-03-24 18:19:03 +0800

    m201

---
+++ src/main/java/com/lwt/testgit4/Test2.java
@@ -0,0 +1,11 @@
+package com.lwt.testgit4;
+
+/**
+ * Created by liu.wentao on 2021-03-24 18:18
+ */
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("m201");
+    }
+}
