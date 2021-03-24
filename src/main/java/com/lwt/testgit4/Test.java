3e5303e30d528f35968bb787dc3e5b7d7e3c5b49
liuwentao <liu.wentao@immomo.com>
2021-03-24 17:29:10 +0800

    test

--- src/main/java/com/lwt/testgit4/Test.java
+++ src/main/java/com/lwt/testgit4/Test.java
@@ -6,36 +6,6 @@ package com.lwt.testgit4;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
-        System.out.println("6");
-        System.out.println("7");
-        System.out.println("8");
-        System.out.println("m1");
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println("m2");
-        System.out.println("t1");
-        System.out.println("m3");
-        System.out.println("t2");
-        System.out.println("t3");
-        System.out.println("t5");
-        System.out.println("t6");
-        System.out.println("t7");
-        System.out.println("t8");
-        System.out.println("t9");
-        System.out.println("t10");
-        System.out.println("t11");
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
-        System.out.println("6");
-        System.out.println("7");
-        System.out.println("8");
+
     }
 }
