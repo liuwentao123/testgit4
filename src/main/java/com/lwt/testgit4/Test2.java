45848eaa6002ed955600dcaea17e1fecf27858d6
刘文涛 <liuwentao05@kuaishou.com>
2021-07-06 17:40:13 +0800

    t72

--- src/main/java/com/lwt/testgit4/Test2.java
+++ src/main/java/com/lwt/testgit4/Test2.java
@@ -8,5 +8,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("t81");
         System.out.println("t71");
+        System.out.println("t72");
     }
 }
